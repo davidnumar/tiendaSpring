3eae63c11f82fd95e33f51c1b99eb2969e4750a6
Caleala Iorgu <mdcaleala@minsait.com>
2022-12-09 10:23:46 +0100

    asd

--- src/main/java/com/ejercicio/tienda/service/ProductoServiceImpl.java
+++ src/main/java/com/ejercicio/tienda/service/ProductoServiceImpl.java
@@ -2,9 +2,13 @@ package com.ejercicio.tienda.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.ejercicio.tienda.repository.ProductoRepository;
+
 public class ProductoServiceImpl implements ProductoService{
 	
 	@Autowired
 	private ProductoRepository repositorio;
+	
+	
 
 }
