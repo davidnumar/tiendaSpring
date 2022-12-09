ee68f2f90996d616428db0f536c388b85719593c
Alejandro <alejandroserranoromero@gmail.com>
2022-12-09 10:51:55 +0100

    cambios product

--- src/main/java/com/ejercicio/tienda/service/VentaServiceImpl.java
+++ src/main/java/com/ejercicio/tienda/service/VentaServiceImpl.java
@@ -2,32 +2,68 @@ package com.ejercicio.tienda.service;
 
 import java.util.List;
 
-import com.ejercicio.tienda.entities.Venta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ejercicio.tienda.entities.Venta;
+import com.ejercicio.tienda.repository.VentaRepository;
+
+@Service
 public class VentaServiceImpl implements VentaService{
 
+	@Autowired
+	private VentaRepository repositorio;
+	
+	//GET ALL
 	@Override
+	@Transactional(readOnly = true)
 	public List<Venta> mostrarVentas() {
 		// TODO Auto-generated method stub
-		return null;
+		return (List<Venta>) repositorio.findAll();
 	}
 
+	//GET
 	@Override
+	@Transactional(readOnly = true)
 	public Venta buscarVenta(long id) {
 		// TODO Auto-generated method stub
-		return null;
+		return repositorio.findById(id).orElse(null);
 	}
 
+	//CREATE
 	@Override
+	@Transactional
 	public Venta guardarVenta(Venta venta) {
 		// TODO Auto-generated method stub
-		return null;
+		return repositorio.save(venta);
 	}
 
+	//BORRAR
 	@Override
+	@Transactional
 	public Venta borrarVenta(long id) {
 		// TODO Auto-generated method stub
-		return null;
+		Venta ventaBorrada = buscarVenta(id);
+		repositorio.deleteById(id);
+		
+		return ventaBorrada;
+	}
+
+	//UPDATE
+	@Override
+	@Transactional
+	public Venta updateVenta(long id, Venta venta) {
+		 Venta v = this.buscarVenta(id);
+		 v.setCantidad(venta.getCantidad());
+		 v.setCliente(venta.getCliente());
+		 v.setFolio(venta.getFolio());
+		 v.setIva(venta.getIva());
+		 v.setProducto(venta.getProducto());
+		 v.setSubtotal(venta.getSubtotal());
+		 v.setTotal(venta.getSubtotal());
+		 this.guardarVenta(v);
+		 return v;
 	}
 
 }
