bf90db551038fad3ebcd0a1e1bbe865a88e4900c
Alejandro <alejandroserranoromero@gmail.com>
2022-12-09 11:31:15 +0100

    controller product

--- src/main/java/com/ejercicio/tienda/repository/ProductoRepository.java
+++ src/main/java/com/ejercicio/tienda/repository/ProductoRepository.java
@@ -1,9 +1,11 @@
 package com.ejercicio.tienda.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ejercicio.tienda.entities.Producto;
 
+@Repository
 public interface ProductoRepository  extends CrudRepository<Producto, Long>{
 
 }
