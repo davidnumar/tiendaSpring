fbbdeab0c03f35910dcd1047a5017c519c9daf22
dcaceresr <dcaceresr@11LAP5CD2385HYJ.indra.es>
2022-12-09 09:45:43 +0100

    Añadido impl de venta

--- src/main/java/com/ejercicio/tienda/service/VentaServiceImpl.java
+++ src/main/java/com/ejercicio/tienda/service/VentaServiceImpl.java
@@ -1,5 +1,33 @@
 package com.ejercicio.tienda.service;
 
+import java.util.List;
+
+import com.ejercicio.tienda.entities.Venta;
+
 public class VentaServiceImpl implements VentaService{
+
+	@Override
+	public List<Venta> mostrarVentas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Venta buscarVenta(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Venta guardarVenta(Venta venta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Venta borrarVenta(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
