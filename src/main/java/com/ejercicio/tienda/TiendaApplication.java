1dafe2f9b577c1973716bb4e379aaf96b3110608
Picón Vaz <dpicon@minsait.com>
2022-12-09 11:32:34 +0100

    swagger

--- src/main/java/com/ejercicio/tienda/TiendaApplication.java
+++ src/main/java/com/ejercicio/tienda/TiendaApplication.java
@@ -3,7 +3,11 @@ package com.ejercicio.tienda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "APIREST VENTAS", version="1.0",description="Crud de clientes, productos y ventas"))
 public class TiendaApplication {
 
 	public static void main(String[] args) {
