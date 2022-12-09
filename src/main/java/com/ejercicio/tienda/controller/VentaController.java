bbd3295052f3108701a1cab4ad5918fa41e000c1
Núñez Martínez <dnunezm@minsait.com>
2022-12-09 11:00:20 +0100

    update venta

--- src/main/java/com/ejercicio/tienda/controller/VentaController.java
+++ src/main/java/com/ejercicio/tienda/controller/VentaController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -70,4 +71,25 @@ public class VentaController {
 		
 		return new ResponseEntity<Map<String,Object>>(response,HttpStatus.CREATED);
 	}
+	
+	@PutMapping("ventas/{id}")
+	public ResponseEntity<?> save( @PathVariable long id , @RequestBody Venta venta) {
+		
+		Map<String,Object> response =new HashMap<>();
+		Venta v = null;
+		try {
+			this.servicio.updateVenta(id, venta);
+		} catch (DataAccessException e) {
+			response.put("mensaje","Error al editar la venta");
+			response.put("error",e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String,Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		response.put("mensaje","El cliente ha sido editado con éxito!");
+		response.put("venta",v);
+		
+		return new ResponseEntity<Map<String,Object>>(response,HttpStatus.OK);
+	}
+	
+	
 }
