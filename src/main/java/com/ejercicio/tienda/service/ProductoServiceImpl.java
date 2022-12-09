ae432dfbb88c800501086ca4dd8c0d60a8990f1d
Núñez Martínez <dnunezm@minsait.com>
2022-12-09 10:19:48 +0100

    prop

--- src/main/java/com/ejercicio/tienda/service/ProductoServiceImpl.java
+++ src/main/java/com/ejercicio/tienda/service/ProductoServiceImpl.java
@@ -2,6 +2,8 @@ package com.ejercicio.tienda.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.ejercicio.tienda.repository.ProductoRepository;
+
 public class ProductoServiceImpl implements ProductoService{
 	
 	@Autowired
